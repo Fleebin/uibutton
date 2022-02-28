c74873588f921cf77addc6975157edc525bbbe6f
Felipe <felipemosilvaa@outlook.com>
2022-02-28 01:03:10 -0300

    Feature: Adicionando os icones aos botões

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { UIButton } from './components/UI/button'
+import './style.scss'
 
 const App = () => {
   return (
     <>
-      <UIButton className="primary">Teste</UIButton>
-      <UIButton className="secondary">Teste</UIButton>
+      <UIButton className="primary">Primario</UIButton>
+      <UIButton className="secondary">Secundario</UIButton>
 
-      <UIButton disabled={true} className="primary">Teste</UIButton>
-      <UIButton disabled={true} className="secondary">Teste</UIButton>
+      <UIButton className="primary" icon="arrow-right"> </UIButton>
+      <UIButton className="primary" icon="check"></UIButton>
+      <UIButton className="primary" icon="refresh"></UIButton>
+
+      <UIButton disabled={true} className="primary">Desabilitado</UIButton>
+      <UIButton disabled={true} className="secondary">Desabilitado</UIButton>
     </>
   );
 }
--- src/components/UI/button.js
+++ src/components/UI/button.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.scss'
+import { ReactComponent as Check } from '../../icons/check.svg'
+import { ReactComponent as Arrow } from '../../icons/arrow.svg'
+import { ReactComponent as Refresh } from '../../icons/refresh.svg'
 
 export const UIButton = ({
-    children,
     className,
     isDisabled,
+    icon,
     ...restProps
 }) => {
     return (
@@ -14,9 +17,11 @@ export const UIButton = ({
             className={`ui-button ${className}`}
             {...restProps}
         >
-            {children}
+            <span>
+                {icon === 'arrow-right' ? <Arrow /> : ''}
+                {icon === 'check' ? <Check /> : ''}
+                {icon === 'refresh' ? <Refresh /> : ''}
+            </span>
         </button>
     );
 };
-
-
